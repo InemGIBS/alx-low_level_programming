bad19811fe848ea139a96aea85d952acb9a0f353
InemGIBS <pstgibson001@gmail.com>
2022-10-25 09:23:54 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * _strlen - returns the length of a string.
+ *
+ * @s: string whose length is to be returned
+ *
+ * Return: length of the string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		{
+			length++;
+
+		}
+	return (length);
+
+}
