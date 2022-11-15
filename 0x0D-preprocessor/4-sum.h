dff6ebd5fc1da25c33b6af4efbe72bffb35f3fcd
InemGIBS <pstgibson001@gmail.com>
2022-11-14 20:29:31 -0800

    fhfhfh

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Auth: InemGIBS
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * @x: the variable x
+ * @y: the variaable y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
