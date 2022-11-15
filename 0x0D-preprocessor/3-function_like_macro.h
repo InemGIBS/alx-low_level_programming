b22c1251016d24db7f4b5685ab21490e424024f9
InemGIBS <pstgibson001@gmail.com>
2022-11-14 20:21:02 -0800

    hot fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,7 +5,6 @@
  * File: 3-function_like_macro.h
  * Auth: InemGIBSON
  * Desc: Header file that defines a function-like macro ABS(x)
- * @x: The x in the code
  * that computes the absolute value of a number x.
  */
 
